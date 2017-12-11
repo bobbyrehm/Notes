38c725ebc1afece2daee46125521726101166bdc
Bobby Rehm <bobbyrehm502@gmail.com>
2017-12-11 08:21:14 -0600

    End of Chapter 6: Core Data and Dependency Injection

---
+++ Notes/Application Delegate/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Notes
+//
+//  Created by Bobby Rehm on 12/11/17.
+//  Copyright © 2017 Bobby Rehm. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    // MARK: - Properties
+
+    var window: UIWindow?
+
+    // MARK: - Application Life Cycle
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
---
+++ Notes/Managers/CoreDataManager.swift
@@ -0,0 +1,67 @@
+//
+//  CoreDataManager.swift
+//  Notes
+//
+//  Created by Bobby Rehm on 12/11/17.
+//  Copyright © 2017 Bobby Rehm. All rights reserved.
+//
+
+import CoreData
+
+final class CoreDataManager {
+    
+    // MARK: - Properties
+    
+    private let modelName: String
+    
+    private(set) lazy var managedObjectContext: NSManagedObjectContext = {
+        let managedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+        managedObjectContext.persistentStoreCoordinator = self.persistentStoreCoordinator
+        return managedObjectContext
+    }()
+    
+    private lazy var managedObjectModel: NSManagedObjectModel = {
+        // Fetch Model URL
+        guard let modelURL = Bundle.main.url(forResource: self.modelName, withExtension: "momd") else {
+            fatalError("Unable to Find Data Model")
+        }
+        
+        // Initialize Managed Object Model
+        guard let managedObjectModel = NSManagedObjectModel(contentsOf: modelURL) else {
+            fatalError("Unable to Load Data Model")
+        }
+        
+        return managedObjectModel
+    }()
+    
+    private lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
+        // Initialize Persistent Store Coordinator
+        let persistentStoreCoordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
+        
+        // Helpers
+        let fileManager = FileManager.default
+        let storeName = "\(self.modelName).sqlite"
+        
+        // URL Documents Directory
+        let documentsDirectoryURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        
+        // URL Persistent Store
+        let persistentStoreURL = documentsDirectoryURL.appendingPathComponent(storeName)
+        
+        do {
+            // Add Persistent Store
+            let options = [ NSMigratePersistentStoresAutomaticallyOption : true, NSInferMappingModelAutomaticallyOption : true ]
+            try persistentStoreCoordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: persistentStoreURL, options: options)
+        } catch {
+            fatalError("Unable to Add Persistent Store")
+        }
+        
+        return persistentStoreCoordinator
+    }()
+    
+    // MARK: - Initialization
+    
+    init(modelName: String) {
+        self.modelName = modelName
+    }
+}
